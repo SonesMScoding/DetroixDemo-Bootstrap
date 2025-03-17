73bc78a93b95b4dc366fe8de453db2385285d0ec
SonesMScoding <eve.sones@mscoding.org>
2025-03-17 10:22:22 -0500

    changes

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -12,13 +12,11 @@ function showSlides(n) {
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].classList.remove("active");
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].classList.add("active");
   dots[slideIndex-1].className += " active";
 }
-
-// This is also from W3, but I removed the class that is correlated to the pointers, so it is just dot selection
